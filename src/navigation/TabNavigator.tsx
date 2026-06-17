c822c11ebdd9c375d630b321a9b03f776742d919
Ahmed Ragab <ahmedragab.se@gmail.com>
2026-06-17 04:39:26 +0300

    conductor(checkpoint): Checkpoint end of Phase 5

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen } from '../screens/HomeScreen';
 import { SettingsScreen } from '../screens/SettingsScreen';
+import { DemoScreen } from '../screens/DemoScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,6 +10,7 @@ export const TabNavigator = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Demo" component={DemoScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
   );
